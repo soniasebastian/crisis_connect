dafbb384c2f60dfc7ebbbdecc893fab2f6b21c06
soniasebastian <soniasebastian011@gmail.com>
2023-09-20 16:56:21 -0400

    added userinfo

--- crisis-connect/src/App.js
+++ crisis-connect/src/App.js
@@ -1,14 +1,19 @@
 import Home from './components/HomePage';
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
 import Header from './components/Header'
+import UserInfo from './components/Userinfo';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
        <Home/>
+       <userInfo/>
    
    <Routes>
      <Route path='/' element={<Home/>}/>
+     <Route path= '/userInfo' element= {<UserInfo/>}/>
    </Routes>
     </div>
   );
