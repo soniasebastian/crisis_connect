ea230541ad48cb017f2c441c08c5425010ed4a30
Tsion <tsionwendmu12@gmail.com>
2023-09-21 16:28:27 -0400

    s

--- crisis-connect/src/App.js
+++ crisis-connect/src/App.js
@@ -1,22 +1,24 @@
-import Home from './components/HomePage';
+
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import UserInfo from './components/Userinfo';
 import IcanHelp from './components/IcanHelp'
+import Landing from './pages/Landing';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
-      <userInfo />
+
+      {/* <UserInfo /> */}
 
       <Routes>
-        <Route path='/' element={<Home />} />
+        
         <Route path='/userInfo' element={<UserInfo />} />
         <Route path='/icanhelp' element={<IcanHelp />} />
+        <Route path='/' element={<Landing/>}/>
       </Routes>
     </div>
   );
