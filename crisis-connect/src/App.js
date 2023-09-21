9d8c655a275b5b3483cd3028732737c6df9017d1
theron-t <chilong.ca22@gmail.com>
2023-09-21 17:12:15 -0400

    IneedHelp -> UserInfoForm

--- crisis-connect/src/App.js
+++ crisis-connect/src/App.js
@@ -1,26 +1,28 @@
 import Home from './components/HomePage';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import UserInfo from './components/Userinfo';
 import IcanHelp from './components/IcanHelp';
 import IneedHelp from "./pages/IneedHelp";
+import UserInfoForm from "./pages/UserInfoForm";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <userInfo />
+    return (
+        <div className="App">
+            <Header/>
+            <Home/>
+            <userInfo/>
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/userInfo' element={<UserInfo />} />
-          <Route path='/ineedhelp' element={<IneedHelp />} />
-        <Route path='/icanhelp' element={<IcanHelp />} />
-      </Routes>
-    </div>
-  );
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/userInfo' element={<UserInfo/>}/>
+                <Route path='/ineedhelp' element={<IneedHelp/>}/>
+                <Route path='/icanhelp' element={<IcanHelp/>}/>
+                <Route path="/userinforeg" element={<UserInfoForm/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
