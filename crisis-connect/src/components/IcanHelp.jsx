800415751d2214dd5ebbb279c180dece5e1f2b72
PaulWong1018 <shuipowong@gmail.com>
2023-09-21 14:16:18 -0400

    canHelp css

--- crisis-connect/src/components/IcanHelp.jsx
+++ crisis-connect/src/components/IcanHelp.jsx
@@ -1,6 +1,13 @@
-export default function IcanHelp () {
-    return ( 
-        <div>IcanHelp</div>
-    )
+import React from "react";
+import "./IcanHelp.css";
 
+export default function IcanHelp() {
+    return (
+        <div className="split-container">
+            <div className="left-panel">123</div>
+            <div className="right-panel">
+
+                </div>
+            </div>
+            );
 }
